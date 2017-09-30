65baf60a6f65e8455206d29d8a0a72918a9e9686
Scott Woodend <woodendscott@gmail.com>
2017-09-30 00:03:37 -0400

    style login form

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
+import '../styles/userdataform.css';
 
 export default class UserDataForm extends Component {
 
   render(){
     let inputs = this.props.inputData.map((inputObj, i) => {
-      return <input type={inputObj.type} placeholder={inputObj.placeholder} required="true"/>
+      return (
+        <input className="form-input" key={i} type={inputObj.type} placeholder={inputObj.placeholder} required="true"/>
+      )
     });
 
-    console.log(inputs);
-
     return (
-      <form>
-        {inputs}
-        <input type="submit" value="Submit"/>
-      </form>
+      <div>
+        <h3>{this.props.title}</h3>
+        <div id="login-form">
+          <form className="user-data-form">
+            {inputs}
+            <input className="submit-button" type="submit" value="Submit"/>
+          </form>
+        </div>
+      </div>
     )
   }
 }
