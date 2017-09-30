d86a2f2f18ce8b881f284a63bf0c60b065beec91
Scott Woodend <woodendscott@gmail.com>
2017-09-30 18:43:35 -0400

    <UserDataForm/> is now stately to keep track of input values being submitted to server. Currently the component renders an HTML <input> tag with an onChange listener. This creates a new function for each render and is not optimal. The UserDataForm component will  eventually render a custom <Input/> component to avoid having to create a new function each time onChange is called. Instead, the child component will run the parent method in UserDataForm, avoiding creating multiple functions

--- src/components/UserDataForm.js
+++ src/components/UserDataForm.js
@@ -3,10 +3,54 @@ import '../styles/userdataform.css';
 
 export default class UserDataForm extends Component {
 
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      emailValue: '',
+      passwordValue: ''
+    }
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    console.log('form was submitted with email', this.state.emailValue);
+    console.log('passowrd', this.state.passwordValue);
+    
+  }
+
+  handleChange(e, inputType){
+    //email or password
+    console.log('handleChange was called with', inputType);
+    console.log('e.target.value', e.target.value);
+    
+    if(inputType === "Email"){
+      this.setState({
+        emailValue: e.target.value
+      });
+    }
+
+    if(inputType="Password"){
+      this.setState({
+        passwordValue: e.target.value
+      });
+    }
+  }
+
   render(){
     let inputs = this.props.inputData.map((inputObj, i) => {
       return (
-        <input className="form-input" key={i} type={inputObj.type} placeholder={inputObj.placeholder} required="true"/>
+        <input 
+          className="form-input" 
+          key={i} type={inputObj.type} 
+          placeholder={inputObj.placeholder} 
+          required="true"
+          onChange={(e) => {
+            this.handleChange(e, inputObj.placeholder);
+          }}
+        />
       )
     });
 
@@ -14,7 +58,7 @@ export default class UserDataForm extends Component {
       <div>
         <h3>{this.props.title}</h3>
         <div id="login-form">
-          <form className="user-data-form">
+          <form onSubmit={this.handleSubmit} className="user-data-form">
             {inputs}
             <input className="submit-button" type="submit" value="Submit"/>
           </form>
