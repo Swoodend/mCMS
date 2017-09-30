99c236f6412a8d50a2df95d7227b3893e80fa545
Scott Woodend <woodendscott@gmail.com>
2017-09-30 16:03:09 -0400

    create signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import UserDataForm from './UserDataForm';
 
 export default class Signup extends Component {
 
   render(){
-
+    let inputs = [
+      {
+        type: "email",
+        placeholder: "Email"
+      },
+      {
+        type: "password",
+        placeholder: "Password"
+      }
+    ];
     return (
-      <h1>Signup Page</h1>
+      <div className="green-hex-background">
+        <div className="form-container">
+          <UserDataForm title="Signup" inputData={inputs}/>
+        </div>
+      </div>
     );
   }
 }
