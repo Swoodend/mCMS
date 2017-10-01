6cddef17bf87834c3be607c70fd7ce34d7b53a93
Scott Woodend <woodendscott@gmail.com>
2017-10-01 17:59:43 -0400

    create user model

---
+++ db/UserModel.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema ({
+  email: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  articles: {type: Array, default: []}
+});
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User: User
+}
