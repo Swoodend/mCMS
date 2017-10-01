e0073401ac6a89b9a0a89b8a0f5a2ad6f2840712
Scott Woodend <woodendscott@gmail.com>
2017-10-01 18:01:37 -0400

    redirect user to /dashboard after form submission

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import UserDataForm from './UserDataForm';
+import { Redirect } from 'react-router-dom';
+
 import '../styles/login.css';
 
 export default class Login extends Component {
+
+  constructor(props){
+    super(props);
+    this.redirectAfterLogin = this.redirectAfterLogin.bind(this);
+    
+    this.state = {
+      redirectToDash : false
+    }
+  }
+
+  redirectAfterLogin(){
+    this.setState({
+      redirectToDash: true
+    });
+  }
 
   render(){
     let inputs = [
@@ -16,10 +33,13 @@ export default class Login extends Component {
       }
     ];
 
+    let redirectAfterLogin = this.state.redirectToDash ? <Redirect to="/dashboard"/> : null;
+
     return (
       <div className="green-hex-background">
+        {redirectAfterLogin}
         <div className="form-container">
-          <UserDataForm title="Login" inputData={inputs}/>
+          <UserDataForm title="Login" inputData={inputs} redirectUser={this.redirectAfterLogin}/>
         </div>
       </div>
     )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import UserDataForm from './UserDataForm';
+import { Redirect } from 'react-router-dom';
 
 export default class Signup extends Component {
+
+  constructor(props){
+    super(props);
+    this.redirectAfterLogin = this.redirectAfterLogin.bind(this);
+    
+    this.state = {
+      redirectToDash : false
+    }
+  }
+
+  redirectAfterLogin(){
+    this.setState({
+      redirectToDash: true
+    });
+  }
+
 
   render(){
     let inputs = [
@@ -14,10 +31,12 @@ export default class Signup extends Component {
         placeholder: "Password"
       }
     ];
+    let redirectAfterSignup = this.state.redirectToDash ? <Redirect to="/dashboard"/> : null;
     return (
       <div className="green-hex-background">
+        {redirectAfterSignup}
         <div className="form-container">
-          <UserDataForm title="Signup" inputData={inputs}/>
+          <UserDataForm title="Signup" inputData={inputs} redirectUser={this.redirectAfterLogin}/>
         </div>
       </div>
     );
