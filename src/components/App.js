f2f70f94cf28e07aca46c56595f370a9938406b2
Scott Woodend <woodendscott@gmail.com>
2017-10-01 18:00:38 -0400

    render Dashboard component. Also Navbar now manages it's own state so no need to pass props to it

--- src/components/App.js
+++ src/components/App.js
@@ -4,34 +4,37 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Signup from './Signup';
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 
 export default class App extends Component {
 
   constructor(props){
     super(props);
+    this.checkLogin = this.checkLogin.bind(this);
+
     this.state = {
       loggedIn: false
     }
   }
 
-  componentWillMount(){
-    let loggedIn = localStorage.getItem("currentUser");
-    if (loggedIn){
-      this.setState({
-        loggedIn: true
-      });
-    }
+
+
+  checkLogin(){
+    return localStorage.getItem("currentUser");
   }
 
   render(){
       return (
         <BrowserRouter>
           <div>
-            <Navbar/>
+            <Navbar checkLoginState={this.checkLogin}/>
             <Route exact path="/" component={Home}/>
             <Route path="/login" component={Login}/>
             <Route path="/signup" component={Signup}/>
+            <Route path='/dashboard' render={() => (
+              <Dashboard checkLoginState={this.checkLogin} />
+            )}/>
           </div>
         </BrowserRouter>
       );
