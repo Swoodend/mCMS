a64ba551c26f00714b5ce8386efdb8ee07f86fc2
Scott Woodend <woodendscott@gmail.com>
2017-10-01 11:56:40 -0400

    routes accept user form information to be saved to DB

--- server.js
+++ server.js
@@ -2,16 +2,29 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// app.use(express.static(__dirname + '/public'));
 
-app.get('/api/login', (req, res) => {
+app.post('/api/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log('in login', email, password)    
     res.json({"status": "OK"});
 })
+
+app.post('/api/signup', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log('in signup', email, password)
+    res.json({"status":"OK"});
+})
+
 
 app.listen(port, () => { 
     console.log('app listening on', port);
