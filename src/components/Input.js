828bacc040674a457d3af3bd234a8c4a621b3bdf
Scott Woodend <woodendscott@gmail.com>
2017-10-04 20:05:40 -0400

    switch tabsize to 2 from 4

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 
 export default class Input extends Component {
 
-    constructor(props){
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
+	constructor(props){
+		super(props);
+		this.handleChange = this.handleChange.bind(this);
+	}
 
-    handleChange(e, inputType){
-        this.props.updateParentState(e, this.props.type);
-    }
+	handleChange(e, inputType){
+		this.props.updateParentState(e, this.props.type);
+	}
 
-    render(){
-        return (
-            <input 
-                onChange={this.handleChange}
-                className={this.props.className}
-                type={this.props.type}
-                placeholder={this.props.placeholder}
-                required="true"    
-            />
-        )
-    }
+	render(){
+		return (
+			<input 
+					onChange={this.handleChange}
+					className={this.props.className}
+					type={this.props.type}
+					placeholder={this.props.placeholder}
+					required="true"    
+			/>
+		)
+	}
 }
