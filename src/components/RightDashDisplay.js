36c8588bba6388fe27526adb9bb0599714d1ef60
Scott Woodend <woodendscott@gmail.com>
2017-10-05 13:22:47 -0400

    RightDashDisplay will eventually render components based on what the user requests. Currently just render DashUserDisplay

---
+++ src/components/RightDashDisplay.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import DashUserDisplay from './DashUserDisplay';
+
+export default class RightDashDisplay extends Component {
+	render(){
+		return (
+			<DashUserDisplay/>
+		)
+	}
+}
