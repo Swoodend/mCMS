5f13fe5fe7df421823d3116909c650a7542b708b
Scott Woodend <woodendscott@gmail.com>
2017-10-05 15:49:00 -0400

    RightDashDisplay now renders the view past into it through Dashboard's state

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import LeftNav from './LeftNav';
 import RightDashDisplay from './RightDashDisplay';
+import DashUserDisplay from './DashUserDisplay';
+import NewContentInfoPage from'./NewContentInfoPage';
 import "../styles/dashboard.css";
 
 export default class Dashboard extends Component{
 
 	constructor(props){
 		super(props);
+		this.setNewView = this.setNewView.bind(this);
 		this.state  = {
-				loggedIn: false
+				loggedIn: false,
+				rightDashView: <DashUserDisplay/>
 		}
 	}
 
@@ -22,6 +26,12 @@ export default class Dashboard extends Component{
 		}
 			
 	}
+
+	setNewView(view){
+		this.setState({
+			rightDashView: view
+		})
+	}
     
 
 	render(){
@@ -29,8 +39,8 @@ export default class Dashboard extends Component{
 		return (
 			<div className="main-dash-container">
 				{redirect}
-				<LeftNav/>
-				<RightDashDisplay/>
+				<LeftNav changeView={this.setNewView} />
+				<RightDashDisplay currentView={this.state.rightDashView}/>
 			</div>
 		)
 	}
