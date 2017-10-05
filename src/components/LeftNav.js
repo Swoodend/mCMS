1570b1a48fd1031600ae6cf9f361c52813d70272
Scott Woodend <woodendscott@gmail.com>
2017-10-04 20:06:11 -0400

    switch tabsize to 2 from 4

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -1,46 +1,56 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import "../styles/leftnav.css";
 
 export default class LeftNav extends Component {
 
-    constructor(props){
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-        this.state = {
-            collapsed : true
-        }
-    }
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+				collapsed : true
+		}
+	}
 
-    handleClick(){
-        this.setState({
-            collapsed: !this.state.collapsed
-        });
-    }
-    
-    render(){
-
-        return (
-            <div className="left-nav-container">
-                <div onClick={this.handleClick} className="one">
-                    <i id="pencil-icon" className="fa fa-pencil"/>New<i id="chev-down" className="fa fa-chevron-down"/>
-                </div>
-                {!this.state.collapsed && 
-                <div>
-                    <div className="two">
-                        <i className="fa fa-chevron-right"/>Blog
-                    </div>
-                    <div className="two">
-                        <i className="fa fa-chevron-right"/>Article
-                    </div>
-                    <div className="two">
-                        <i className="fa fa-chevron-right"/>Newsletter
-                    </div>        	  
-                </div>
-                }  
-                <div className="one"><i id="np-icon" className="fa fa-newspaper-o"/>My Content</div>
-                <div className="one"><i id="gear-icon" className="fa fa-gear"/>My Account</div>
-            </div>
-        )
-    }
+	handleClick(){
+		this.setState({
+			collapsed: !this.state.collapsed
+		});
+	}
+	
+	render(){
+		let chevronDirection = this.state.collapsed ? 'down' : 'up';
+		return (
+			<div className="left-nav-container">
+				<div onClick={this.handleClick} className="one">
+					<i id="pencil-icon" className="fa fa-pencil"/>New<i id="chev-down" className={`fa fa-chevron-${chevronDirection}`}/>
+				</div>
+				{!this.state.collapsed && 
+				<div>
+					<Link to="/new/blog">
+						<div className="two">
+							<i className="sub-chev fa fa-chevron-right"/><span className="text-1">Blog</span>
+						</div>
+					</Link>
+					<Link to="/new/article">
+						<div className="two">
+							<i className="sub-chev fa fa-chevron-right"/><span className="text-2">Article</span>
+						</div>
+					</Link>
+					<Link to="/new/newsletter">
+						<div className="two">
+							<i className="sub-chev fa fa-chevron-right"/><span className="text-3">Newsletter</span>
+						</div>
+					</Link>
+				</div>
+				}
+				<Link to="/content">  
+					<div className="one"><i id="np-icon" className="fa fa-newspaper-o"/>My Content</div>
+				</Link>
+				<Link to="/account">
+					<div className="one"><i id="gear-icon" className="fa fa-gear"/>My Account</div>
+				</Link>
+			</div>
+		)
+	}
 }
