e3df7c138bf47d995c9ccaec949987281dda5ef9
Scott Woodend <woodendscott@gmail.com>
2017-10-04 20:05:09 -0400

    switch tabsize to 2 from 4

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,34 +5,34 @@ import "../styles/dashboard.css";
 
 export default class Dashboard extends Component{
 
-    constructor(props){
-        super(props);
-        this.state  = {
-            loggedIn: false
-        }
-    }
+	constructor(props){
+		super(props);
+		this.state  = {
+				loggedIn: false
+		}
+	}
 
-    componentWillMount(){
-        let loggedIn = this.props.checkLoginState();
-        if (loggedIn){
-            this.setState({
-                loggedIn: true
-            })
-        }
-        
-    }
+	componentWillMount(){
+		let loggedIn = this.props.checkLoginState();
+		if (loggedIn){
+				this.setState({
+						loggedIn: true
+				})
+		}
+			
+	}
     
 
-    render(){
-        let redirect = this.state.loggedIn ? null : <Redirect to="/login" />
-        return (
-            <div className="main-dash-container">
-                {redirect}
-                <LeftNav/>
-                <div className="right-display-area">
+	render(){
+		let redirect = this.state.loggedIn ? null : <Redirect to="/login" />
+		return (
+			<div className="main-dash-container">
+				{redirect}
+				<LeftNav/>
+				<div className="right-display-area">
 
-                </div>
-            </div>
-        )
-    }
+				</div>
+			</div>
+		)
+	}
 }
