77aea95ca0d5a8a82184c99dda184f254d423b09
Scott Woodend <woodendscott@gmail.com>
2017-10-05 13:04:29 -0400

    create Account component for future use

---
+++ src/components/Account.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/account.css';
+
+export default class Account extends Component {
+	render(){
+		return (
+			<div className="main-account-container">
+				<h1>Welcome to the Account Page</h1>
+			</div>
+		)
+	}
+}
