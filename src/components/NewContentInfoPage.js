8cf410f86af94aa92a9df7717b24ab90ed23f08e
Scott Woodend <woodendscott@gmail.com>
2017-10-05 15:50:43 -0400

    Create component to eventually display info about each form of content you can create

---
+++ src/components/NewContentInfoPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/newcontentinfopage.css';
+
+export default class NewContentInfoPage extends Component {
+    render(){
+        return (
+            <div className="new-content-info-container">
+                <h1>hi its the new content info page</h1>
+            </div>
+        )
+    }
+}
