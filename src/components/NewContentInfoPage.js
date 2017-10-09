6ca5ba9db8b27d00e01ebe07298b0117b09e0eb2
Scott Woodend <woodendscott@gmail.com>
2017-10-09 01:39:45 -0400

    component now renders info page about what kinds of content users can create

--- src/components/NewContentInfoPage.js
+++ src/components/NewContentInfoPage.js
@@ -5,7 +5,40 @@ export default class NewContentInfoPage extends Component {
     render(){
         return (
             <div className="new-content-info-container">
-                <h1>hi its the new content info page</h1>
+                <div className="icon-row">
+                    <i className="fa fa-pencil-square-o fa-5x"/>
+                    <i className="fa fa-comment-o fa-5x"/>
+                    <i className="fa fa-newspaper-o fa-5x"/>
+                </div>
+                <div className="descriptor-container">
+                    <div className="descriptor">
+                        <span className="leading-word-big">Blog -</span>
+                        <br/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                            sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </div>
+                    <div className="descriptor">
+                        <span className="leading-word-big">Article -</span>
+                        <br/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                            sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </div>
+                    <div className="descriptor">
+                        <span className="leading-word-big">Newsletter -</span>
+                        <br/>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                            sunt in culpa qui officia deserunt mollit anim id est laborum
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }
