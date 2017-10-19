29a00b22bf42b19911cb23efefaa5b10ca0672eb
Scott Woodend <woodendscott@gmail.com>
2017-10-18 22:16:21 -0400

    create ContentPage component

---
+++ src/components/ContentPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ContentPage extends Component {
+    render() {
+        return (
+            <h1>Welcome to the content page</h1>
+        )
+    }
+}
