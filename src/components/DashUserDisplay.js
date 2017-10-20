77d9e5ce324516433ff78840b170da400cc535fb
Scott Woodend <woodendscott@gmail.com>
2017-10-19 22:43:31 -0400

    DashUserDisplay now will open/close a modal that will prompt the user with a form to change their profile picture

--- src/components/DashUserDisplay.js
+++ src/components/DashUserDisplay.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import UploadImageModal from './UploadImageModal';
 import '../styles/dashuserdisplay.css';
 
 export default class DashUserDisplay extends Component {
+	
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+		this.state = {
+			displayingUploadModal:  false
+		}
+	}
+
+	handleClick(){
+		this.setState({
+			displayingUploadModal: !this.state.displayingUploadModal
+		})
+	}
+
 	render(){
 		let currentUser = localStorage.getItem("currentUser");
+		let { displayingUploadModal } = this.state;
+	
 		return (
 			<div className="main-userdisplay-container">
 
@@ -14,6 +32,11 @@ export default class DashUserDisplay extends Component {
 						className="profile-img" 
 						src={this.props.profileImg || "http://via.placeholder.com/250x250"}
 				/>
+				<div className="change-image-container">
+					<div onClick={this.handleClick} className="change-image-button">Change Image</div>
+				</div>
+
+				{displayingUploadModal && <UploadImageModal closeModal={this.handleClick}/>}
 
 			</div>
 		)
