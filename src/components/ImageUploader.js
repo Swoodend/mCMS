5f697c63e9e14d614170247ac9162b6db5389fa0
Scott Woodend <woodendscott@gmail.com>
2017-10-21 21:38:20 -0400

    ImageUploader submits form data Via fetch API now instead of html form. Once the image is saved, a function will set the new profile image for the user

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import '../styles/imageuploader.css';
 
 export default class ImageUploader extends Component {
+    
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        console.log("submit registered");
+        let user = localStorage.getItem('currentUser');
+        let form = new FormData(e.target);
+        form.append('user', user);    
+
+        let requestConfig = {
+            method: "POST",
+            body: form,
+          };
+
+        fetch('/api/upload', requestConfig)
+        .then((res) => {
+            return res.json();
+        }).then((res) => {
+            console.log('everything good');
+        });
+    }
+
+
     render() {
         return (
             <div className="image-uploader-container">
-                <form id="image-upload-form" method="POST" encType="multipart/form-data" action="/api/upload">
+                <form onSubmit={this.handleSubmit}id="image-upload-form" method="POST" encType="multipart/form-data" action="/api/upload">
                     <input type="file" name="avatar"/>
                     <input type="submit" value="Upload"/>
                 </form>
