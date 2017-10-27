70b5bd7b5caf4f1271f70343e52a85256f7c30f1
Scott Woodend <woodendscott@gmail.com>
2017-10-26 21:54:07 -0400

    Usermodel defaults to a 250x250 placeholder image if no avatar is set

--- db/UserModel.js
+++ db/UserModel.js
@@ -6,7 +6,7 @@ let userSchema = new Schema ({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   articles: {type: Array, default: []},
-  avatar: {type: String}
+  avatar: {type: String, default: "http://via.placeholder.com/250x250"}
 });
 
 let User = mongoose.model('User', userSchema);
