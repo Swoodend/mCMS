9754bd872000277ede8131b3b0888be08339ee2c
Scott Woodend <woodendscott@gmail.com>
2017-10-28 09:33:02 -0400

    require FS so it does not need to be passed in as an argument to the function. Modules are cached the first time they are loaded by node so this is not an issue.

--- helpers/image_upload/image_helpers.js
+++ helpers/image_upload/image_helpers.js
@@ -1,5 +1,6 @@
+const fs = require('fs');
 
-function moveFileAndSend(fs, oldPath, newPath, responseObj, res){
+function moveFileAndSend(oldPath, newPath, responseObj, res){
     //moves an image file to path and sends responseObj to be handled by client
     fs.rename(oldPath, newPath, () => {
         res.send(responseObj);
