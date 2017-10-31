09855185431f0599d6cb89ad36ec9edbcd8431f1
Scott Woodend <woodendscott@gmail.com>
2017-10-30 22:11:33 -0400

    AvatarThumbnail displays one of the users avatars that they have uploaded. When clicked, their current avatar will be set to the image that they click upon

---
+++ src/components/AvatarThumbnail.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class AvatarThubmnail extends Component {
+    constructor(props){
+        super(props);
+        this.setNewAvatar = this.setNewAvatar.bind(this);
+    }
+
+    setNewAvatar(){
+        this.props.setNewAv(this.props.fileName);
+    }
+
+    render(){
+        return (
+            <img
+            className="user-uploaded-avatar" 
+            key={this.props.keyprop}
+            src={this.props.src}
+            onClick={this.setNewAvatar}
+        />
+        )
+    }
+}
