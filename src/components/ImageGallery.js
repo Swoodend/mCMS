9e41edfd16c1a52ffcbb7502667211a167157f8d
Scott Woodend <woodendscott@gmail.com>
2017-10-30 22:15:14 -0400

    ImageGallery will now render all of the users previously uploaded avatars. Eventually when a user clicks an avatar image this will become their new avatar

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
+import AvatarThumbnail from './AvatarThumbnail';
 import '../styles/imagegallery.css';
 
 export default class ImageGallery extends Component {
+    
+    constructor(props){
+        super(props);
+        this.setNewAvatar = this.setNewAvatar.bind(this);
+    }
+
+    setNewAvatar(fileName){
+        console.log('this was called!', fileName);
+    }
+
     render(){
-        let divStyle = {"background":"red", "height": "100px", "width": "100px", "outline":"1px solid black"}
+        console.log('ImageGallery', this.props.avatars);
+        let { avatars, email } = this.props;
+        let srcPrefix = 'http://localhost:3001/public/uploads/' + email
+        console.log(avatars, email);
+        console.log(srcPrefix);
+
+        if (avatars.length && email){
+            avatars = avatars.map((fileName, i) => {
+                return (
+                    <AvatarThumbnail
+                        key={i}
+                        src={srcPrefix + '/' + fileName}
+                        fileName={fileName}
+                        setNewAv={this.setNewAvatar}
+                    />
+                )
+            })
+        } else {
+            avatars = null;
+        }
+
         return (
             <div className="image-gallery-content-container">
                 <div className="image-gallery-subcontainer">
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>
-                    <div style={divStyle}></div>                    
+                     {avatars}
                 </div>
             </div>
         )
