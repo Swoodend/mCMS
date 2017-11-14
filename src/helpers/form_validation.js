791eb8cf0f062410676c2f7f328561c5e6a3c456
Scott Woodend <woodendscott@gmail.com>
2017-11-14 17:56:19 -0500

    validateBlogForm function returns an emtpy errorHash if the form is submitted correctly, otherwise, the input that contains the error is returned. A flash message will be displayed to the user eventually (when they submit the form)

--- src/helpers/form_validation.js
+++ src/helpers/form_validation.js
@@ -1,5 +1,24 @@
-export function validateBlogForm(){
-    console.log("VALIDATING THE BLOG FORM");
+export function validateBlogForm(form){
+    //form['burl'].value ---> the value of the Blog URL input
+    let requiredInputs = ['burl', 'blog-header', 'bbc'];
+    let urlRegex =  /[^0-9a-zA-Z-_]/g;
+    let errorHash = {};
+
+    //check that all required fields have some value
+    for (let i = 0; i < requiredInputs.length; i++){
+        let currentInput = requiredInputs[i];
+        if (form[currentInput].value === ""){
+            errorHash[currentInput] = "is a required value"
+            return errorHash
+        }
+    }
+    //otherwise check that blog URL has acceptable format
+    let formHasBadUrl = form['burl'].value.match(urlRegex)
+    if (formHasBadUrl){
+        errorHash['burl'] = `url cannot contain ${formHasBadUrl[0]}`
+    }
+
+    return errorHash;
 }
 
 export function validateNewsletterForm(){
