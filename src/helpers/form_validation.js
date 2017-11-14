17a553a962fa4772b67a636a010ed63f7e6f9f41
Scott Woodend <woodendscott@gmail.com>
2017-11-14 16:28:56 -0500

    create validation functions for each different type of content (blog, newsletter, article)

---
+++ src/helpers/form_validation.js
@@ -0,0 +1,15 @@
+export function validateBlogForm(){
+    console.log("VALIDATING THE BLOG FORM");
+}
+
+export function validateNewsletterForm(){
+    console.log("VALIDATING THE NEWSLETTER FORM");
+}
+
+
+export function validateArticleForm() {
+    console.log("VALIDATING THE ARTICLE FORM");
+}
+
+
+
