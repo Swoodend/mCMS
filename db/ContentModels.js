89a9cd647f82ea15580dca78800ad281fdb530bd
Scott Woodend <woodendscott@gmail.com>
2017-11-16 13:23:29 -0500

    creates a contentSchema and exports the model. Content model will be used to save blogs, articles and newsletters to the db

---
+++ db/ContentModels.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const contentSchema = new Schema({
+    type: { type: String, required: true},
+    creator: { type: String, required: true},
+    url: { type: String, required: true, unqiue: true },
+    barColorOne: { type: String },
+    barColorTwo: { type: String },
+    barColorThree: { type: String },
+    barColorFour: { type: String },
+    carouselImages: { type: Array, default: [] },
+    primaryImage: { type: String },
+    secondaryImageOne: { type: String },
+    secondaryImageTwo: { type: String },    
+    miniNavTextOne: { type: String },
+    miniNavHrefOne: { type: String },
+    miniNavTextTwo: { type: String },
+    miniNavHrefTwo: { type: String },
+    miniNavTextThree: { type: String },
+    miniNavHrefThree: { type: String },
+    facebookUrl : { type: String },
+    instagramUrl : { type: String },
+    snapchatUrl : { type: String },
+    contact: {type: String },
+    address: { type: String },
+    generalLinkHrefOne: { type: String },
+    generalLinkTextOne: { type: String },
+    generalLinkHrefTwo: { type: String },
+    generalLinkTextTwo: { type: String }
+});
+
+let Content = mongoose.model('Content', contentSchema);
+
+module.exports = {
+    Content : Content
+}
